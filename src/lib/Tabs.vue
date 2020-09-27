58e9da12bffb6d1d0d335ec009e14b42d321a847
grea-t <1193976273@qq.com>
2020-09-27 16:21:42 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="gulu-tabs">
-    <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}"
-           :key="index">{{t}}
+    <div class="gulu-tabs-nav" ref="container">
+      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (el) navItems[index] = el }"
+           @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}
       </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults"
@@ -13,7 +14,7 @@
 </template>
 <script lang="ts">
   import Tab from './Tab.vue'
-  import {computed} from 'vue'
+  import {computed, ref, onMounted, onUpdated} from 'vue'
 
   export default {
     props: {
@@ -22,6 +23,28 @@
       }
     },
     setup(props, context) {
+      const navItems = ref<HTMLDivElement[]>([])
+      const indicator = ref<HTMLDivElement>(null)
+      const container = ref<HTMLDivElement>(null)
+      const x = () => {
+        const divs = navItems.value
+        const result = divs.filter(div => div.classList.contains('selected'))[0]
+        console.log(result)
+        const {
+          width
+        } = result.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = result.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      }
+      onMounted(x)
+      onUpdated(x)
       const defaults = context.slots.default()
       defaults.forEach((tag) => {
         if (tag.type !== Tab) {
@@ -29,7 +52,6 @@
         }
       })
       const current = computed(() => {
-        console.log('重新 return')
         return defaults.filter((tag) => {
           return tag.props.title === props.selected
         })[0]
@@ -40,7 +62,7 @@
       const select = (title: string) => {
         context.emit('update:selected', title)
       }
-      return {defaults, titles, current, select}
+      return {defaults, titles, current,  select, navItems, indicator, container}
     }
   }
 </script>
@@ -53,6 +75,7 @@
       display: flex;
       color: $color;
       border-bottom: 1px solid $border-color;
+      position: relative;
 
       &-item {
         padding: 8px 0;
@@ -66,6 +89,16 @@
         &.selected {
           color: $blue;
         }
+      }
+
+      &-indicator {
+        position: absolute;
+        height: 3px;
+        background: $blue;
+        left: 0;
+        bottom: -1px;
+        width: 100px;
+        transition: all 250ms;
       }
     }
 
