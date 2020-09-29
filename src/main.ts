f128bf3ae54eb1831a50a5059ffe5618b6875e9a
grea-t <1193976273@qq.com>
2020-09-29 17:59:59 +0800

    优化 markdown

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import "./lib/gulu.scss";
 import "./index.scss";
-import { createApp } from "vue";
+import {createApp} from "vue";
 // @ts-ignore
 import App from "./App.vue";
 // @ts-ignore
-import { router } from './router';
+import {router} from './router';
 import 'github-markdown-css'
+// @ts-ignore
+import Markdown from './components/Markdown.vue';
 
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
+app.component("Markdown", Markdown)
--- src/router.ts
+++ src/router.ts
@@ -13,14 +13,12 @@ import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 // @ts-ignore
 import DocDemo from "./components/DocDemo.vue";
+import { h } from 'vue';
 // @ts-ignore
-import Intro from './views/Intro.vue';
-// @ts-ignore
-import GetStarted from './views/GetStarted.vue'
-// @ts-ignore
-import Install from './views/Install.vue'
+import Markdown from './components/Markdown.vue';
 
 const history = createWebHashHistory();
+const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 export const router = createRouter({
   history: history,
   routes: [
@@ -30,9 +28,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         {path: "", component: DocDemo},
-        {path: "intro", component: Intro},
-        {path: "get-started", component: GetStarted},
-        {path: "install", component: Install},
+        { path: "intro", component: md('intro') },
+        { path: "get-started", component: md('get-started') },
+        { path: "install", component: md('install') },
         {path: "switch", component: SwitchDemo},
         {path: "button", component: ButtonDemo},
         {path: "dialog", component: DialogDemo},
