96c2984ae5c35c701590f7907ea80e76e84dfdec
grea-t <1193976273@qq.com>
2020-10-01 17:36:10 +0800

    优化官网

--- src/router.ts
+++ src/router.ts
@@ -11,8 +11,6 @@ import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 // @ts-ignore
 import TabsDemo from "./components/TabsDemo.vue";
-// @ts-ignore
-import DocDemo from "./components/DocDemo.vue";
 import { h } from 'vue';
 // @ts-ignore
 import Markdown from './components/Markdown.vue';
@@ -27,7 +25,7 @@ export const router = createRouter({
       path: "/doc",
       component: Doc,
       children: [
-        {path: "", component: DocDemo},
+        { path: "", redirect: '/doc/intro' },
         { path: "intro", component: md('intro') },
         { path: "get-started", component: md('get-started') },
         { path: "install", component: md('install') },
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -50,6 +50,7 @@
   }
 </script>
 <style lang="scss" scoped>
+  $aside-index : 10;
   .layout {
     display: flex;
     flex-direction: column;
@@ -91,6 +92,7 @@
     left: 0;
     padding: 70px 0 16px;
     height: 100%;
+    z-index: $aside-index;
 
     > h2 {
       margin-bottom: 4px;
