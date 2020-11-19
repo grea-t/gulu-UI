ec40ecc98a6e6aeb52112b56ed16e461925d3264
grea-t <1193976273@qq.com>
2020-11-19 15:22:10 +0800

    创建 rollup.config.js

---
+++ src/lib/index.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+export { default as Switch } from './Switch.vue';
+// @ts-ignore
+export { default as Button } from './Button.vue';
+// @ts-ignore
+export { default as Tabs } from './Tabs.vue';
+// @ts-ignore
+export { default as Tab } from './Tab.vue';
+// @ts-ignore
+export { default as Dialog } from './Dialog.vue';
+export { openDialog as openDialog } from './openDialog';
